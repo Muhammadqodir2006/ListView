ce4466e956d0496cc0a4ef1e16eaaea96ba479dd
PC672 <rmuhammadqodir58@gmail.com>
2023-02-22 11:27:01 +0500

    init

--- app/src/main/java/uz/itschool/listview/MainActivity.kt
+++ app/src/main/java/uz/itschool/listview/MainActivity.kt
@@ -1,11 +1,88 @@
 package uz.itschool.listview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import uz.itschool.listview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var names = arrayOf(
+        "Elnur",
+        "Farrux",
+        "Diyor",
+        "Behruz",
+        "Muhammmadqodir",
+        "Shamsiddin",
+        "Doniyor",
+        "Shohrux",
+        "Hasan",
+        "Elnur",
+        "Farrux",
+        "Diyor",
+        "Behruz",
+        "Muhammmadqodir",
+        "Shamsiddin",
+        "Doniyor",
+        "Shohrux",
+        "Hasan",
+        "Elnur",
+        "Farrux",
+        "Diyor",
+        "Behruz",
+        "Muhammmadqodir",
+        "Shamsiddin",
+        "Doniyor",
+        "Shohrux",
+        "Hasan",
+        "Elnur",
+        "Farrux",
+        "Diyor",
+        "Behruz",
+        "Muhammmadqodir",
+        "Shamsiddin",
+        "Doniyor",
+        "Shohrux",
+        "Hasan",
+        "Elnur",
+        "Farrux",
+        "Diyor",
+        "Behruz",
+        "Muhammmadqodir",
+        "Shamsiddin",
+        "Doniyor",
+        "Shohrux",
+        "Hasan",
+        "Elnur",
+        "Farrux",
+        "Diyor",
+        "Behruz",
+        "Muhammmadqodir",
+        "Shamsiddin",
+        "Doniyor",
+        "Shohrux",
+        "Hasan",
+        "Elnur",
+        "Farrux",
+        "Diyor",
+        "Behruz",
+        "Muhammmadqodir",
+        "Shamsiddin",
+        "Doniyor",
+        "Shohrux",
+        "Hasan",
+
+
+        )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val arrayAdapter =
+            ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, names)
+        binding.listView.adapter = arrayAdapter
+
     }
 }
---
+++ app/src/main/java/uz/itschool/listview/User.kt
@@ -0,0 +1,3 @@
+package uz.itschool.listview
+
+data class User(var name:String, var login:String, var phone:String, var photo:String)
